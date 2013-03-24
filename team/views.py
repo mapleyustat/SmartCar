8a7c6913b0a6d102ab8b9a13327df41ebdff46e3
everpcpc <everpcpc@live.cn>
2013-03-24 20:27:19 +0800

    参赛队伍

--- team/views.py
+++ team/views.py
@@ -1,4 +1,20 @@
 from django.shortcuts import render
+from team.models import Team
+from django.db.models import ObjectDoesNotExist, Q
 
 def index(request):
-    return render(request, 'team/index.html',{})
+    s=request.GET.get('s')
+    if s:
+        team_list=Team.objects.filter(Q(name__contains=s) | Q(intro__contains=s)).order_by('-create_time')
+        return render(request, 'team/search.html',{'s':s,'team_list':team_list})
+    team_list=Team.objects.order_by('-create_time')
+    return render(request, 'team/index.html',{'team_list':team_list})
+
+def detail(request):
+    pass
+
+def create(request):
+    pass
+
+def edit(request):
+    pass
